af73129982d0006e5ee90ba50f33595ac41524ff
Щукин Павел Владимирович <p_shchukin@RTKIT.local>
2023-07-19 22:17:46 +0300

    task029 made

---
+++ task029/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+int [] number = new int[8];
+int [] Fill(int[] num)
+{
+    for (int i = 0; i < num.Length; i++)
+    {
+    num[i] = new Random().Next(0, 10);
+    Console.Write($"{num[i]} ");
+    }
+    return num;
+}
+
+// Console.Write(Fill(number)); - почему-то выходит System32
+
+void Print(int[] num)
+{
+    for (int i = 0; i < num.Length; i++);    
+}
+Print(Fill(number));
